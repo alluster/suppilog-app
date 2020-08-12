0bffaf80ef3d2f9aeab6370cae1c5b6cd8b21442
Aleksanteri Heliövaara <aleksanteriheliovaara@AleksanterisMBP.home>
2020-08-12 12:29:38 +0300

    transitions added

--- client/src/components/Navigation/index.js
+++ client/src/components/Navigation/index.js
@@ -12,6 +12,25 @@ import { device } from '../../device';
 
 const logo = '/suppilog_logo_horizontal_dark.png'
 
+const NavContainer = styled.div`
+	position: fixed;
+	min-width: 100%;
+	z-index: 10000;
+	height: 50px;
+	text-align: center;
+	top: 0;
+	margin-bottom: -40px;
+	background-color: #fff;
+	box-shadow:
+	2.8px 0 2.2px rgba(0, 0, 0, 0.034),
+	6.7px 0 5.3px rgba(0, 0, 0, 0.048),
+	12.5px 0 10px rgba(0, 0, 0, 0.06),
+	22.3px 0 17.9px rgba(0, 0, 0, 0.072),
+	41.8px 0 33.4px rgba(0, 0, 0, 0.086),
+	100px 0 80px rgba(0, 0, 0, 0.12);
+`;
+
+
 const Icon = styled(FontAwesomeIcon)`
 	font-size: 24px;
 	text-align: right;
@@ -44,22 +63,6 @@ const Text = styled.div `
 
 `;
 
-const NavContainer = styled.div`
-	position: fixed;
-	min-width: 100%;
-	height: 50px;
-	text-align: center;
-	top: 0;
-	margin-bottom: -40px;
-	background-color: #fff;
-	box-shadow:
-	2.8px 0 2.2px rgba(0, 0, 0, 0.034),
-	6.7px 0 5.3px rgba(0, 0, 0, 0.048),
-	12.5px 0 10px rgba(0, 0, 0, 0.06),
-	22.3px 0 17.9px rgba(0, 0, 0, 0.072),
-	41.8px 0 33.4px rgba(0, 0, 0, 0.086),
-	100px 0 80px rgba(0, 0, 0, 0.12);
-`;
 
 const OpenNav = styled.div`
 	top: 0;
