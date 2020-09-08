1629a440f76be5bb9d2538be79ee5df51c2ddd3c
Aleksanteri Heliövaara <aleksanteriheliovaara@AleksanterisMBP.home>
2020-09-08 16:18:57 +0300

    card design

--- client/src/Views/Product/index.js
+++ client/src/Views/Product/index.js
@@ -4,7 +4,7 @@ import HeroSmall from '../../components/HeroSmall';
 import styled from 'styled-components';
 import { AppContext } from '../../context/Context';
 import BannerWithImage from '../../components/BannerWithImage';
-
+import Accordion from '../../components/Accordion'
 
 
 
@@ -33,18 +33,21 @@ const Product = () => {
 			<BannerWithImage 
 				image={product.photo_url}
 				title={product.name}
-				ingress={product.vendor}
+				ingress={product.product_family_name}
 			
 				buttonText=""
 				
 			
 				>
-					<p>{context.t('product.consumer_package_size')}</p>
-					<h3>{product.consumer_package_size},</h3>
-					<p>{context.t('product.sales_unit_size')}: </p>
-					<h3>
-						{product.sales_unit_size} 
-					</h3>
+					
+					<Accordion title={context.t('page.product.accordion-title')} >
+						<p>{context.t('product.consumer_package_size')}</p>
+						<h3>{product.consumer_package_size},</h3>
+						<p>{context.t('product.sales_unit_size')}: </p>
+						<h3>
+							{product.sales_unit_size} 
+						</h3>
+					</Accordion>
 				</BannerWithImage>
 
 			
---
+++ client/src/components/Accordion/index.js
@@ -0,0 +1,79 @@
+import React, { useContext, useState } from 'react';
+import styled from 'styled-components';
+import LoginForm from '../LoginForm';
+import { device } from '../../device';
+import { AppContext } from '../../context/Context';
+import Input from '../Input';
+import Button from '../Button';
+import { FontAwesomeIcon } from '../../../node_modules/@fortawesome/react-fontawesome';
+import { faHeart, faTruck, faCaretDown, faCaretUp, faAngleDown, faAngleUp } from '@fortawesome/free-solid-svg-icons'
+
+const Banner = ({title, children}) => {
+	const context = useContext(AppContext)
+	const [open, setOpen] = useState(false)
+	const AccordionContainer = styled.div`
+	
+	`;
+	
+	const AccordionLine= styled.div`
+		border-top: solid 1px black;
+		width: 100%;
+		display: flex;
+		flex-direction: row;
+		margin-top: 30px;
+
+	`;	
+	
+	const AccordionContent = styled.div`
+		width: 100%;
+
+	`;
+	const Icon = styled(FontAwesomeIcon)`
+		justify-content: flex-end;
+		flex: 1;
+		font-size: 30px;
+	`;	
+	const IconContainer = styled.div`
+		align-items: flex-end;
+
+	`;
+
+	const Title = styled.h3`
+		flex: 1;
+	`;
+
+
+
+    return(
+		<AccordionContainer >
+			<AccordionLine onClick={e => setOpen(true)}>
+				<Title>
+					{title}
+				</Title>
+				<IconContainer>
+					{
+						open ?
+					<Icon icon={faAngleDown} onClick={e => setOpen(false)} />
+					:
+					<Icon icon={faAngleUp} onClick={e => setOpen(true)} />
+					}
+				</IconContainer>
+				
+
+			</AccordionLine>
+			{
+				open ?
+				<AccordionContent onClick={e => setOpen(false)}>
+					<p>{children}</p>
+				</AccordionContent>
+				: 
+				null	
+		}
+		</AccordionContainer>
+       
+    );
+};
+
+ 
+
+export default Banner;
